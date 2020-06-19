c14037ef975ae6fbdfbf25de98a256f5f88d3996
Luis Henrique Rocha Bueno <luizbueno1201@gmail.com>
2020-06-19 17:05:49 -0300

    migrado para Postegres

--- api_teste/settings.py
+++ api_teste/settings.py
@@ -89,8 +89,14 @@ WSGI_APPLICATION = 'api_teste.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'teste_api'),
+        'USER': os.environ.get('DB_USER', 'covid'),
+        'PASSWORD': os.environ.get('DB_PASS', 'CovidChato'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
